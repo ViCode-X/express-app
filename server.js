685653385b10dc3ce056cee489f59b11588bdc38
ViCode-X <adebiyivictor5555@gmail.com>
2026-07-01 16:33:34 +0000

    wk2-node-express

--- server.js
+++ server.js
@@ -4,18 +4,26 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => res.send("Welcome to User API!"));
-
-app.post('/register', (req, res) => {
-    const { name, email } = req.body;
-    if (!name || !email) return res.status(400).json({ error: 'Missing Fields' });
-        //Simulate DB save
-    res.status(201).json({ message: `Registered: ${name} (${email})`});   
+app.use((req, res, next) => {
+  //logs every request
+  console.log(`${req.method} ${req.url} - ${new Date()}`);
+  next();// Pass to next handler (required!)
 });
 
-app.get('/users', (req, res) => {
+app.get('/', (req, res) => res.send("My Week 2 API!"));
+
+app.post('/user', (req, res) => {
+    const { name, email } = req.body;
+    if (!name || !email) return res.status(400).json({ error: 'Missing Data' });
+        //Simulate DB save
+    res.status(201).json({ message: `Hello ${name}`});   
+});
+
+app.get('/user/:id', (req, res) => {
     // Simulate fetching users from DB
-    res.json({id: req.params.id, name: "Sample User" });
+    const id = req.params.id;
+    res.send(`User ${id} profile`);
+    console.log(`User ID: ${id} profile requested`);
 });
 
 app.listen(3000, () => console.log('API live on port 3000'));
